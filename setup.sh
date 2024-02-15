bdfff61e7400e1be9e5d440775ba93d9e16901d9
Makhtar <muazzamakhtar@gmail.com>
2024-02-15 22:26:57 +0400

    Created Scripts for virtual evnvironment

---
+++ setup.sh
@@ -0,0 +1,15 @@
+python3 -m pip install --user --upgrade pip
+if command -v apt-get &> /dev/null; then
+    sudo apt-get install python3-venv
+    sudo apt install python3-tk
+fi
+python3 -m venv virtual_env
+source virtual_env/bin/activate
+if [[ "$VIRTUAL_ENV" != "" ]]; then
+    echo "env created!"
+    # pip install -r requirements.txt
+else
+    echo "env is not working!"
+fi
+
+# sudo apt install portaudio19-dev
