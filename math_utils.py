843fa83cac38f9b074beaaf87f17ff56a02c7de5
Muazzam Akhtar <muazzamakhtar@gmail.com>
2024-02-18 14:46:50 +0400

    Update math_utils.py

--- math_utils.py
+++ math_utils.py
@@ -2,22 +2,86 @@ import math
 import numpy as np
 
 def	getAverage(_list: list) -> float:
-    return sum(_list) / len(_list)
+	
+	"""
+	Calculates the average of the given list.
+	
+	Arg: _list: list
+
+	Returns the float value.
+	"""
+
+	return sum(_list) / len(_list)
 
 def	getDistance(diff_x: float, y1: float, y2: float) -> float:
-    return (round(math.sqrt(math.pow(y2 - y1, 2) + math.pow(diff_x, 2)), 3))
+	
+	"""
+	Calculates the distance of two coordinates.
+	
+	Arg: diff_x: float, y1: float, y2: float
+
+	Returns the float value.
+	"""
+
+	return (round(math.sqrt(math.pow(y2 - y1, 2)
+        + math.pow(diff_x, 2)), 3))
 
 def	getPercentile(data: list, percent: float) -> float:
-    return np.percentile(data, percent)
+	
+	"""
+	Calculates the percentile of the given data.
+	
+	Arg: data: list, percent: float
+
+	Returns the float value.
+	"""
+
+	return np.percentile(data, percent)
 
 def	getMean(data: list) -> float:
-    return np.mean(data)
+
+	"""
+	Calculates the mean of the given list.
+	
+	Arg: data: list
+
+	Returns the float value.
+	"""
+
+	return np.mean(data)
 
 def	getMedian(data: list) -> float:
-    return np.median(data)
+
+	"""
+	Calculates the median of the given list.
+	
+	Arg: data: list
+
+	Returns the float value.
+	"""
+
+	return np.median(data)
 
 def	getStandardDeviation(data: list) -> float:
-    return np.std(data)
+
+	"""
+	Calculates the standard deviation of the given list.
+	
+	Arg: data: list
+
+	Returns the float value.
+	"""
+
+	return np.std(data)
 
 def	getVariance(data: list) -> float:
-    return np.var(data)
+
+	"""
+	Calculates the variance of the given list.
+	
+	Arg: data: list
+
+	Returns the float value.
+	"""
+
+	return np.var(data)
