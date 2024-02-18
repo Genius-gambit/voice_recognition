6323a89a77071c0bec4b145319b1152af6544e3d
Muazzam Akhtar <muazzamakhtar@gmail.com>
2024-02-18 14:47:22 +0400

    Update record_mic.py

--- record_mic.py
+++ record_mic.py
@@ -3,13 +3,27 @@ import wave
 from time import sleep
 
 def	startRecording(filename: str):
+
+	"""
+	Records a user and stores the signal waves .wav file with passed filename.
+	
+	Arg: filename: str
+	"""
+
+	while(1):
+		sleep(2)
+		break
+
 	FRAMES_PER_BUFFER = 3200
 	FORMAT = pyaudio.paInt16
 	CHANNELS = 1
 	RATE = 16000
 
+	print("--> Started Recording <--\n")
 	p = pyaudio.PyAudio()
 
+	seconds = 5
+	frames = []
 	stream = p.open(
 		format=FORMAT,
 		channels=CHANNELS,
@@ -17,15 +31,13 @@ def	startRecording(filename: str):
 		input=True,
 		frames_per_buffer=FRAMES_PER_BUFFER
 	)
-	sleep(1)
-	print("Start Recording")
-
-	seconds = 5
-	frames = []
-	for i in range(0, int(RATE / FRAMES_PER_BUFFER * seconds)):
+	for i in range(0, int(RATE / FRAMES_PER_BUFFER
+        	* seconds)):
 		data = stream.read(FRAMES_PER_BUFFER)
 		frames.append(data)
-	print("Finished Recording")
+		print(f"\r{(int(seconds - ((i + 1) / 5))):02d} \
+Seconds Left", end="", flush=True)
+	print("\n\n--> Finished Recording <--")
 	stream.stop_stream()
 	stream.close()
 	p.terminate()
