4dd60f04f52a5d881fc42286a31fb99779ca0a41
Muazzam Akhtar <muazzamakhtar@gmail.com>
2024-02-18 14:46:10 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,58 +1,67 @@
 from sound_utils import *
 from math_utils import *
-from record_mic import startRecording
+# from plotting import plotting
 
 import matplotlib.pyplot as plt
-import numpy as np
+from file_path_utils import *
 
 def	VoiceValidation() -> bool:
-    
-	src = 'src.wav'
-	startRecording('src.wav')
+	
+	"""
+	Fetches the wave signals of both registered user
+	and the current user.
+
+	Returns the boolean value true if the accuracy of
+	both voice patterns is 90%, else false.
+	"""
+	
+	src = 'voices/Munavar/Recording2.wav'
+	_user = input("Name of the User: ")
+	if os.path.exists(_user) == False:
+		os.mkdir(_user)
+	_command = input("Which command would you\
+	like to do? -> ")
+
+	createRecordingFiles(_user + "/" + _command)
+	
+	_valid = []
 	filename = 'Output.wav'
-	startRecording('Output.wav')
+	startRecording(filename)
+	for i in range(1, 6):
+		src = getPath(_user + "/" + _command + "/"
+				+ i.__str__() + ".wav")
 
-	samples0, sr0, signal0 = getWaves(filename)
-	samples_sf0 = 0
+		samples0, sr0, signal0 = getWaves(filename)
+		samples_sf0 = 0
 
-	try:
-		samples_sf0 - signal0[:, 0]
-	except:
-		samples_sf0 = signal0
-		
-	samples1, sr1, signal1 = getWaves(src)
-	samples_sf1 = 0
+		try:
+			samples_sf0 - signal0[:, 0]
+		except:
+			samples_sf0 = signal0
+			
+		samples1, sr1, signal1 = getWaves(src)
+		samples_sf1 = 0
 
-	try:
-		samples_sf1 - signal1[:, 0]
-	except:
-		samples_sf1 = signal1
-		
-	data0 = [convertToDecibels(i) for i in samples_sf0]
-	data1 = [convertToDecibels(i) for i in samples_sf0]
+		try:
+			samples_sf1 - signal1[:, 0]
+		except:
+			samples_sf1 = signal1
 
-	nt0 = getNormalisedList(samples0)
-	nt1 = getNormalisedList(samples1)
+		samples0, samples_sf0, data0 = filteringNoise(samples0,
+			samples_sf0, [convertToDecibels(i) for i in samples_sf0])
+		samples1, samples_sf1, data1 = filteringNoise(samples1,
+			samples_sf1, [convertToDecibels(i) for i in samples_sf0])
 
-	count = 0
-	startPos0 = nt0.index(1.0)
-	startPos1 = nt1.index(1.0)
+		nt0 = getNormalisedList(samples0)
+		nt1 = getNormalisedList(samples1)
+		count = ComparingWaves(nt0, nt1)
 
-	if startPos0 <= startPos1:
-		count = compareLeftWaves(nt0, startPos0, nt1, startPos1)
-	elif startPos0 > startPos1:
-		count = compareLeftWaves(nt1, startPos1, nt0, startPos0)
-		
-	iterators0 = len(nt0) - startPos0
-	iterators1 = len(nt1) - startPos1
-
-	if iterators0 <= iterators1:
-		count += compareRightWaves(nt0, startPos0 + 1, nt1, startPos1 + 1)
-	elif iterators0 > iterators1:
-		count += compareRightWaves(nt1, startPos1 + 1, nt0, startPos0 + 1)
-
-	print(f"{count/len(data0) * 100}%")
-	return True if count/len(data0) * 100 > 80.0 else False
+		print(f"{count/len(data0) * 100:2.2f}%")
+		_valid.append(True if count/len(data0) * 100\
+			> 85.0 else False)
+		if (i == 5):
+			return True if len([i for i in _valid if i == True])\
+			>= 3 else False
 
 if __name__ == '__main__':
 	print(VoiceValidation())
